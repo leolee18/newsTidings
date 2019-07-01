cac7f07752699c186af63a0df35b6a764b7203d5
leolee18 <wwwliyuxi@163.com>
2019-07-01 21:35:26 +0800

    图片超宽、标题分割

--- src/views/Detailed.vue
+++ src/views/Detailed.vue
@@ -5,7 +5,7 @@
     	<span class="home-title-text">{{headText}}</span>
     </div>
 	<div class="deta-cont">
-		<div class="deta-cont-title">{{detaPage && detaPage.title}}</div>
+		<div class="deta-cont-title">{{title}}<span>{{title2}}</span></div>
 		<div class="deta-cont-date">来源：{{detaPage && detaPage.keyword?detaPage.keyword:'麻姑集团'}}<span class="deta-cont-dleft">发布时间：{{dateFormat(detaPage && detaPage.addtime)}}</span></div>
 		<p class="deta-p" v-html="detaPage && detaPage.content"></p>
 	</div>
@@ -16,11 +16,30 @@
 import {mapGetters} from 'vuex';
 	
 export default {
+	data () {
+		return {
+			title:'',
+			title2:''
+		}
+	},
 	computed:{
 		...mapGetters([
 			'headText',
 			'detaPage'
 		])
+	},
+	watch: {
+		detaPage(val){
+			if(val && val.title){
+				let mArr = val.title.split('——');
+				if(mArr.length >0){
+					this.title = mArr[0];
+				}
+				if(mArr.length >1){
+					this.title2 = '——'+mArr[1];
+				}
+			}
+		}
 	},
 	mounted(){
 		var self = this;
@@ -28,6 +47,16 @@ export default {
 		
 		let mId = self.$route.params.id;
 		self.$store.dispatch('initDetaPage',{id:mId});
+		
+		if(self.detaPage && self.detaPage.title){
+			let mArr = self.detaPage.title.split('——');
+			if(mArr.length >0){
+				this.title = mArr[0];
+			}
+			if(mArr.length >1){
+				this.title2 = '——'+mArr[1];
+			}
+		}
 	},
 	methods: {
 		dateFormat(mTime) {
@@ -49,7 +78,6 @@ export default {
 	.deta-all{
 		position:relative;
 		width: 750px;
-		
 		background-color: #fff;
 	}
 	.home-title{
@@ -81,17 +109,16 @@ export default {
 	
 	.deta-cont{
 		position:relative;
-		width: 750px;
+		width: 670px;
 		padding: 40px;
-		box-sizing: border-box;
 		font-size: 16px;
 		color: #666;
 		line-height: 30px;
+		overflow: hidden;
 	}
 	.deta-cont img{
 		position: relative;
-		display: block;
-		width: 100%;
+		width: 50%;
 		height: auto;
 	}
 	.deta-cont-title{
@@ -101,6 +128,12 @@ export default {
 		color: #333;
 		text-align: center;
 		line-height: 50px;
+	}
+	.deta-cont-title span{
+		display: block;
+		font-size: 20px;
+		line-height: 40px;
+		color: #666;
 	}
 	.deta-cont-date{
 		position: relative;
@@ -114,6 +147,8 @@ export default {
 	}
 	.deta-p{
 		position: relative;
+		width: 100%;
 		margin-top: 20px;
+		overflow: hidden;
 	}
 </style>
